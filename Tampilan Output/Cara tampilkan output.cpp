958d134c97dd1cbcf6594ee4f9f35f88df99118b
Najmuzahir1147 <122434356+Najmuzahir1147@users.noreply.github.com>
2023-01-16 17:24:37 +0700

    Update Cara tampilkan output.cpp

--- Tampilan Output/Cara tampilkan output.cpp
+++ Tampilan Output/Cara tampilkan output.cpp
@@ -1,9 +1,11 @@
+//ini adalah file headernya
 #include<stdio.h>
 #include<conio.h>
 #include<iostream.h>
 main()
 {
 clrscr();
+//cara menampilkan kalimat yang kita tulis pada layar
 cout<<"Kami Sedang Belajar C++"<<endl;
 cout<<"Mari Bergabung dengan kami";
 getch();
