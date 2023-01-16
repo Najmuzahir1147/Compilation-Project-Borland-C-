0283cf6b2c57755cba8f57436440c473701d6aef
Najmuzahir1147 <122434356+Najmuzahir1147@users.noreply.github.com>
2023-01-16 17:16:14 +0700

    Add files via upload

---
+++ Tampilan Output/Cara tampilkan output.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+main()
+{
+clrscr();
+cout<<"Kami Sedang Belajar C++"<<endl;
+cout<<"Mari Bergabung dengan kami";
+getch();
+}
---
+++ Tampilan Output/contoh output.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream.h>
+main()
+{
+clrscr();
+cout<<"              BIODATA PRIBADI SAYA           "<<endl;
+cout<<"============================================="<<endl;
+cout<<"Nama Lengkap    : Najmuzahir Munawa"<<endl;
+cout<<"Kota Kelahiran  : Ciamis"<<endl;
+cout<<"Tanggal Lahir   : 30 September 2006"<<endl;
+cout<<"Alamat          : Panyingkiran"<<endl;
+cout<<"No.Telp         : 089661223802"<<endl;
+getch();
+}
