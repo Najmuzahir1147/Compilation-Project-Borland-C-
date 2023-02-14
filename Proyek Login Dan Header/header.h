25e0cf18a78f6b0fcf6b2f9cd6e82f53fe732b17
Nazbro <122434356+Najmuzahir1147@users.noreply.github.com>
2023-02-14 07:07:59 +0700

    Add files via upload

---
+++ Proyek Login Dan Header/header.h
@@ -0,0 +1,13 @@
+progra(char judd[49]="")
+{
+if(strcmp(judd,"HEADER")==0)
+{
+cout<<"===================Biodata====================="<<endl;
+cout<<"Nama  : Najmuzahir Munawar"<<endl;
+cout<<"Kelas : X-PPLG"<<endl;
+cout<<"User  : Najmuzahir"<<endl;
+cout<<"Pass  : arnproject23"<<endl;
+cout<<"=============Masukan User dan Pass=============="<<endl;
+getch();
+}
+}
